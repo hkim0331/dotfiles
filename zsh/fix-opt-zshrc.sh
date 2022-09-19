318afd7cd0528fa37854507354a8528190e3b3bb
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-20 08:19:04 +0900

    add comments on fix-opt-zshrc.sh

--- zsh/fix-opt-zshrc.sh
+++ zsh/fix-opt-zshrc.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-SH='/opt/homebrew/opt/zsh-git-prompt/zshrc.sh'
-if [[ `grep python3 ${SH}` ]]; then
-    echo using pyton3
+#
+# Using bash explicitly.
+# In macos, /bin/sh is not a symlink to /bin/bash.
+# /bin/sh errors in bellow script.
+
+RC='/opt/homebrew/opt/zsh-git-prompt/zshrc.sh'
+
+if [[ `grep python3 ${RC}` ]]; then
+    echo ok using pyton3
 else
-    gsed -i.bak 's/python/python3/g' ${SH}
-    echo fix
+    gsed -i.bak 's/python/python3/g' ${RC}
+    echo fixed
 fi
