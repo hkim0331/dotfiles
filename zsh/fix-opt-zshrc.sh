87414813474d0bd5c4b51a2afcfbea9e4c362e0b
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-28 09:29:02 +0900

    use HOMEBREW_PREFIX in fix-opt-zshrc.sh

--- zsh/fix-opt-zshrc.sh
+++ zsh/fix-opt-zshrc.sh
@@ -4,7 +4,7 @@
 # In macos, /bin/sh is not a symlink to /bin/bash.
 # /bin/sh errors in bellow script.
 
-RC='/opt/homebrew/opt/zsh-git-prompt/zshrc.sh'
+RC="${HOMEBREW_PREFIX}/opt/zsh-git-prompt/zshrc.sh"
 
 if [[ `grep python3 ${RC}` ]]; then
     echo ok using pyton3
